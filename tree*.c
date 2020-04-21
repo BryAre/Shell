e9bad8d80433eadd9869dec0787350d69a8d610c
lwong0515 <43160578+lwong0515@users.noreply.github.com>
2020-04-20 22:22:12 -0400

    Update tree*.c

--- tree*.c
+++ tree*.c
@@ -15,26 +15,46 @@
 
 int main(int argc, char const *argv[]) {
   int ret1, ret2, file1, file2, file3;
-  // make Dir0
-  ret1 = mkdir("Dir0", S_IRWXU);
+  struct stat st = {0};
+  // check if directory Dir0 exists already
+  if(stat("./Dir0", &st) == -1){
+    // make Dir0
+    ret1 = mkdir("./Dir0", S_IRWXU);
+  }
   // change working directory to Dir0
-  if(chdir("Dir0") != 0){
+  if(chdir("./Dir0") != 0){
     perror("Failed to change directory Dir0!");
   }
   else {
-    // create 3 empty textfiles
+    // create 3 empty text files
     file1 = open("t1.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRWXU);
+    if(-1 == file1){
+      printf("\nCannot create txt1.txt");
+      return 1;
+    }
     file2 = open("t2.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRWXU);
+    if(-1 == file2){
+      printf("\nCannot create txt2.txt");
+      return 1;
+    }
     file3 = open("t3.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRWXU);
-    // close files
-    close(file1);
-    close(file2);
-    close(file3);
-
-    // create Dir1
-    ret2 = mkdir("Dir1", S_IRWXU);
+    if(-1 == file3){
+      printf("\nCannot create txt3.txt");
+      return 1;
+    }
+    // check if directory Dir1 exists already
+    if(stat("./Dir1", &st) == -1){
+      // create Dir1
+      ret2 = mkdir("./Dir1", S_IRWXU);
+    }
+    execlp("ls", "ls", "-F", NULL);
     // confirmation message
-    printf("%s\n", "Change to directory Dir0 and textfiles created successful!");
+    // printf("%s\n", "Change to directory Dir0 and textfiles created successful!");
   }
+  // close files
+  close(file1);
+  close(file2);
+  close(file3);
+
   return 0;
 }
