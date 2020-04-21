a6d2d6650728a55c89eb57a4ef035f705dbbca1d
Alex <lexieguo@gmail.com>
2020-04-20 22:43:12 -0400

    Print ls output to file. Delete use of pipe.

--- list*.c
+++ list*.c
@@ -14,68 +14,35 @@ list*
 #include <errno.h>
 
 int main(int argc, char const *argv[]) {
-  pid_t pid1, pid2;
-  int status1, status2;
-
   int destfd;
-  int count = 1;
-  char buff[100];
+  int pid1, pid2, status;
   errno = 0;
 
-  //Create new file
-  destfd = creat("tree.txt", S_IRWXU);
-
-  //Opens files
+  //Open or create tree.txt file
+  destfd = open("tree.txt", O_WRONLY | O_CREAT, S_IRWXU);
   if(-1 == destfd){
-    printf("\n create files failed with error [%s]\n",strerror(errno));
+    printf("\n open() tree.txt file failed with error [%s]\n",strerror(errno));
     return 1;
   }
-  else{
-    close(destfd);
-  }
 
-  //Create pipe
-  int fd1[2];
-  if (pipe(fd1) != 0) {
-    fprintf(stderr, "Pipe Failed\n");
-  }
-
-  //Fork Child 1
-  //output should be writen to pipe (cannot read)
   pid1 = fork();
-  if (pid1 == 0) { //Fork() success
-    system("clear");
-    close(fd1[0]); //Close input/reading side of pipe
-    dup2(fd1[1],1); //Close stdout; copies and points write-end of pipe to stdout(1)
-    execlp("ls", "ls", "-l", NULL); //execute ls -F; -1 for vertical listing
-
+  if (pid1==0) { //child forked
+    system("clear"); //clear terminal screen
+    execlp("ls", "ls", "-l", NULL); //execute ls -l;
+    printf ("EXECVP Failed\n"); //print only if failed
   }
-  else if (pid1 < 0) {
-    fprintf(stderr, "Child 1 Fork Failed\n");
-    return 1;
-  }
-  //Parent Process
-  else{
-    //Fork Child 2
-    //input should be read from pipe (cannot write)
+  else{ //parent
     pid2 = fork();
-    if (pid2 == 0) { //Fork() success
-      close(fd1[1]); //Close output/writing side of pipe
-      dup2(fd1[0], destfd);
-      
+    if (pid2==0) { //child forked
+      dup2(destfd, 1); //make stdout print to file
+      execlp("ls", "ls", "-l", NULL); //execute ls -l;
+      printf ("EXECVP Failed\n"); //print only if failed
     }
-    else if (pid2 < 0) {
-      fprintf(stderr, "Child 2 Fork Failed\n");
-      return 1;
+    else{
+      waitpid(pid2, &status, 0); //wait for child process to finish
+      close(destfd); //close file
     }
-
-    close(fd1[0]); //Close input/reading side of pipe
-    close(fd1[1]); //Close output/writing side of pipe
-    waitpid(pid1, &status1, 0);
-    // printf("Child 1 Process ended\n");
-    waitpid(pid2, &status2, 0);
-    // printf("Child 2 Process ended\n");
-    // printf("Parent Process ended\n");
+    waitpid(pid1, &status, 0); //wait for child process to finish
   }
   return 0;
 }
